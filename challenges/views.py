97dd6dddb7d1fa9ab5f32fa552828c39940dfcd2
Abdullah Doğan <abdullah.dogan@codeck.com.tr>
2022-04-08 02:28:16 +0300

    links are created to with index view

--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
+
+# Create your views here.
 
 monthly_challenges = {
     "january": "Read 20 pages of paper for the entire mounth!",
@@ -16,7 +19,20 @@ monthly_challenges = {
     "december": "Walk for at least 20 minutes every day!"
 }
 
-# Create your views here.
+
+def index(request):
+    list_items = ""
+    months = list(monthly_challenges.keys())
+
+    for month in months:
+        capitalized_month = month.capitalize()
+        month_path = reverse("month-challenge", args=[month])
+        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
+
+    # "<li><a href="...">June</a></li>"
+
+    response_data = f"<ul>{list_items}</ul>"
+    return HttpResponse(response_data)
 
 
 def monthly_challenge_by_number(request, month):
@@ -25,13 +41,16 @@ def monthly_challenge_by_number(request, month):
     if month > len(months):
         return HttpResponseNotFound("Invalid month")
 
+    # URL path config ==> /challenge/january
     redirect_month = months[month - 1]
-    return HttpResponseRedirect("/challenges/" + redirect_month)
+    redirect_path = reverse("month-challenge", args=[redirect_month])
+    return HttpResponseRedirect(redirect_path)
 
 
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        return HttpResponse(challenge_text)
+        response_data = f"<h1>{challenge_text}</h1>"
+        return HttpResponse(response_data)
     except:
-        return HttpResponseNotFound("Please write one of the months!")
+        return HttpResponseNotFound("<h1>Please write one of the months!</h1>")
